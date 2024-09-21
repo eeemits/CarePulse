f48bfe296b35491cc8ce73087025cd558a8c9909
eemitss <emirfahimi0@gmail.com>
2024-09-21 11:22:19 +0800

    implement registeration feature

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,17 +1,27 @@
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
     const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name);
-    console.log(users);
-    console.log(newUser);
+    return newUser;
   } catch (error: any) {
     if (error && error?.code === 409) {
       const documents = await users.list([Query.equal("email", [user.email])]);
 
       return documents?.users.find((item) => item.email === user.email);
     }
+    throw error;
   }
-  // const results = await
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const response = await users.get(userId);
+
+    return parseStringify(response);
+  } catch (error) {
+    console.log(error);
+  }
 };
