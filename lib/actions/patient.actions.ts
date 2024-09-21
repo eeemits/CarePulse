4e0cf79b4cf08e20716bb7bb9c980998eb1cfe6b
Badi <69509789+eeemits@users.noreply.github.com>
2024-09-21 11:28:02 +0800

    Revert "Dev"

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,1 +1,27 @@
-export const createUser = async (user: CreateUserParams) => {};
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name);
+    return newUser;
+  } catch (error: any) {
+    if (error && error?.code === 409) {
+      const documents = await users.list([Query.equal("email", [user.email])]);
+
+      return documents?.users.find((item) => item.email === user.email);
+    }
+    throw error;
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const response = await users.get(userId);
+
+    return parseStringify(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
