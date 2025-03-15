5ede5f9e2be472ac155028d7cc6c4c0acf38b789
Emir <emirfahimi0@gmail.com>
2025-03-15 16:41:03 +0800

    update lib

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,26 +18,26 @@ export const formatDateTime = (dateString: Date | string) => {
     year: "numeric", // numeric year (e.g., '2023')
     hour: "numeric", // numeric hour (e.g., '8')
     minute: "numeric", // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    hour12: true // use 12-hour clock (true) or 24-hour clock (false)
   };
 
   const dateDayOptions: Intl.DateTimeFormatOptions = {
     weekday: "short", // abbreviated weekday name (e.g., 'Mon')
     year: "numeric", // numeric year (e.g., '2023')
     month: "2-digit", // abbreviated month name (e.g., 'Oct')
-    day: "2-digit", // numeric day of the month (e.g., '25')
+    day: "2-digit" // numeric day of the month (e.g., '25')
   };
 
   const dateOptions: Intl.DateTimeFormatOptions = {
     month: "short", // abbreviated month name (e.g., 'Oct')
     year: "numeric", // numeric year (e.g., '2023')
-    day: "numeric", // numeric day of the month (e.g., '25')
+    day: "numeric" // numeric day of the month (e.g., '25')
   };
 
   const timeOptions: Intl.DateTimeFormatOptions = {
     hour: "numeric", // numeric hour (e.g., '8')
     minute: "numeric", // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    hour12: true // use 12-hour clock (true) or 24-hour clock (false)
   };
 
   const formattedDateTime: string = new Date(dateString).toLocaleString("en-US", dateTimeOptions);
@@ -52,7 +52,7 @@ export const formatDateTime = (dateString: Date | string) => {
     dateTime: formattedDateTime,
     dateDay: formattedDateDay,
     dateOnly: formattedDate,
-    timeOnly: formattedTime,
+    timeOnly: formattedTime
   };
 };
 
@@ -63,3 +63,5 @@ export function encryptKey(passkey: string) {
 export function decryptKey(passkey: string) {
   return atob(passkey);
 }
+
+export const isArrayNotEmpty = (value: unknown[]) => value !== undefined && value !== null && value.length > 0;
--- lib/validation.ts
+++ lib/validation.ts
@@ -9,24 +9,66 @@ const defaultFieldSchema = (min?: number, max?: number) =>
 const defaultDateSchema = z
   .object({
     startDate: z.coerce.date().refine((data) => data > new Date(), { message: "Start date must be in the future" }),
-    endDate: z.coerce.date(),
+    endDate: z.coerce.date()
   })
   .refine((data) => data.endDate > data.startDate, {
     message: "End date cannot be earlier than start date.",
-    path: ["endDate"],
+    path: ["endDate"]
   });
 
 export const userFormValidation = z.object({
   name: z.string().min(2, "Name must be at least 2 characters").max(50, "Name must be at most 50 characters"),
   email: z.string().email("Invalid email address"),
+  phone: z.string().refine((phone) => /^\+\d{10,15}$/.test(phone), "Invalid phone number")
+});
+
+export const patientFormValidation = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters").max(50, "Name must be at most 50 characters"),
+  email: z.string().email("Invalid email address"),
   phone: z.string().refine((phone) => /^\+\d{10,15}$/.test(phone), "Invalid phone number"),
-  address: defaultFieldSchema(1, 250),
-  currentMedication: z.string().min(2, "Value must be at least 2 characters").max(50, "value must be at most 20 characters"),
-  allergies: defaultFieldSchema(4, 20),
-  familyMedication: defaultFieldSchema(10, 250),
-  pastMedicationHistory: defaultFieldSchema(),
-  insurance: defaultFieldSchema(5, 250),
-  occupation: defaultFieldSchema(),
-  primaryCare: defaultFieldSchema(),
-  dateOfBirth: defaultDateSchema,
+  birthDate: z.coerce.date(),
+  gender: z.enum(["Male", "Female", "Other"]),
+  address: z.string().min(5, "Address must be at least 5 characters").max(500, "Address must be at most 500 characters"),
+  occupation: z.string().min(2, "Occupation must be at least 2 characters").max(500, "Occupation must be at most 500 characters"),
+  emergencyContactName: z
+    .string()
+    .min(2, "Contact name must be at least 2 characters")
+    .max(50, "Contact name must be at most 50 characters"),
+  emergencyContactNumber: z
+    .string()
+    .refine((emergencyContactNumber) => /^\+\d{10,15}$/.test(emergencyContactNumber), "Invalid phone number"),
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  insuranceProvider: z
+    .string()
+    .min(2, "Insurance name must be at least 2 characters")
+    .max(50, "Insurance name must be at most 50 characters"),
+  insurancePolicyNumber: z
+    .string()
+    .min(2, "Policy number must be at least 2 characters")
+    .max(50, "Policy number must be at most 50 characters"),
+  allergies: z.string().optional(),
+  currentMedication: z.string().optional(),
+  familyMedicalHistory: z.string().optional(),
+  pastMedicalHistory: z.string().optional(),
+  identificationType: z.string().optional(),
+  identificationNumber: z.string(),
+  identificationDocument: z.custom<File[]>().optional(),
+  treatmentConsent: z
+    .boolean()
+    .default(false)
+    .refine((value) => value === true, {
+      message: "You must consent to treatment in order to proceed"
+    }),
+  disclosureConsent: z
+    .boolean()
+    .default(false)
+    .refine((value) => value === true, {
+      message: "You must consent to disclosure in order to proceed"
+    }),
+  privacyConsent: z
+    .boolean()
+    .default(false)
+    .refine((value) => value === true, {
+      message: "You must consent to privacy in order to proceed"
+    })
 });
